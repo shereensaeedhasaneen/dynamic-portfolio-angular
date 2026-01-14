771ad2b3af69ba3ddb1a0c6689c5b70adb7cd515
donia12saeed <donia789saeed@gmail.com>
2026-01-15 00:47:40 +0200

    Initial commit: Setup Angular 18 with Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}",],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1f242d', 
+        secondary: '#0ef', 
+      }
+    },
+  },
+  plugins: [],
+}
+
