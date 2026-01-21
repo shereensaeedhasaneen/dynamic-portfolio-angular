9cb5c8c7b7d13cf501a8bf1f98a9e0dc7cc108a6
shereensaeedhasaneen <sheerysaeed123@gmail.com>
2026-01-22 00:57:44 +0200

    add routes to navbar links and add class for the active link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { HeroComponent } from "./features/hero/hero.component";
+import { AboutComponent } from "./features/about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, HeroComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, HeroComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './features/hero/hero.component';
+import { AboutComponent } from './features/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'' , component: HeroComponent },
+    { path:'about' , component: AboutComponent },
+    { path: '**', redirectTo: '' }
+];
