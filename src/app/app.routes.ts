a03174ba2b3649a2d010174728bc4524a2ae5c61
shereensaeedhasaneen <sheerysaeed123@gmail.com>
2026-01-22 02:55:33 +0200

    create projects page and add its design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HeroComponent } from './features/hero/hero.component';
 import { AboutComponent } from './features/about/about.component';
 import { SkillsComponent } from './features/skills/skills.component';
+import { ProjectsComponent } from './features/projects/projects.component';
 
 export const routes: Routes = [
     { path:'' , component: HeroComponent },
     { path:'about' , component: AboutComponent },
     { path:'skills' , component: SkillsComponent },
+    { path:'projects' , component: ProjectsComponent },
     { path: '**', redirectTo: '' }
 ];
