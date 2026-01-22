81a1f7e1b3f2605b4731ec17da8790f972f73c3d
shereensaeedhasaneen <sheerysaeed123@gmail.com>
2026-01-23 00:21:46 +0200

    add experience page and its design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HeroComponent } from './features/hero/hero.component';
 import { AboutComponent } from './features/about/about.component';
 import { SkillsComponent } from './features/skills/skills.component';
 import { ProjectsComponent } from './features/projects/projects.component';
+import { ExperienceComponent } from './features/experience/experience.component';
 
 export const routes: Routes = [
     { path:'' , component: HeroComponent },
     { path:'about' , component: AboutComponent },
     { path:'skills' , component: SkillsComponent },
     { path:'projects' , component: ProjectsComponent },
+    { path:'experience' , component: ExperienceComponent },
     { path: '**', redirectTo: '' }
 ];
